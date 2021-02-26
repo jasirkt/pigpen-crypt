a76d2e55868c2b938752879036594834ecf40280
Jasir KT <meetjasir@gmail.com>
2021-02-26 08:06:41 +0530

    re-add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useStyles from './styles';
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Pigpen Crypt
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+   flexGrow: 1,
+ },
+ title: {
+   flexGrow: 1,
+ },
+ appBar: {
+   alignItems: 'center'
+ }
+}));
+
+export default useStyles;
